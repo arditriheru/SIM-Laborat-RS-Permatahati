d3c68b1a50a7d921181b6b5f28b228f9486766e3
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-10-07 16:33:02 +0700

    Add files via upload

--- app/info.php
+++ app/info.php
@@ -21,16 +21,21 @@
         <div class="jumbotron">
           <h2>Laboratorium RSKIA Permata</h2>
           <p>Email  : lab.rskia.permata@gmail.com</p>
-          <p>Telpon : (0274) 415317</p>
+          <p>Telepon : (0274) 415317</p>
         </div>
       </div>
     </div>
     <div class="col-lg-6">
       <div class="bs-example">
         <div class="jumbotron">
-          <h2>IT RSKIA Rachmi</h2>
-          <p>Email  : it.rskiarachmi@gmail.com</p>
-          <p>Telpon : (0274) 415318</p>
+          <h2>Ardi Tri Heru</h2>
+          <p>Email  : arditriheruh@gmail.com</p>
+          <p>Mobile : 0896 2967 1717</p>
+          <p>
+            <a href="https://api.whatsapp.com/send?phone=6289629671717" target="_blank" class="btn btn-success btn-lg"><i class="fa fa-whatsapp"></i></a>
+            <a href="https://www.instagram.com/arditriheru/" target="_blank" class="btn btn-danger btn-lg"><i class="fa fa-instagram"></i></a>
+            <a href="https://gmail.com" target="_blank" class="btn btn-primary btn-lg"><i class="fa fa-envelope"></i></a>
+          </p>
         </div>
       </div>
     </div>
