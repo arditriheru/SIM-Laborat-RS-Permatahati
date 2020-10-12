37f327d394d44315d6ac99a74f2365174343ce8d
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-10-12 14:46:59 +0700

    Add files via upload

--- config/welcome.php
+++ config/welcome.php
@@ -1,4 +1,4 @@
-<div class="alert alert-info alert-dismissable">
+<div class="alert alert-success alert-dismissable">
 	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 	<font size='3'>Selamat Datang di Sistem Informasi Rumah Sakit RSKIA Permata Yogyakarta</font>
 </div>
